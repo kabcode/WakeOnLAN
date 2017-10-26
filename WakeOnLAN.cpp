f49c4dbb54bb62e3a97ed2a9b0daba3e9d00a610
Gordian Kabelitz <kabcode@gmail.com>
2017-10-26 23:58:49 +0200

    read MAC addresses from file

--- WakeOnLAN.cpp
+++ WakeOnLAN.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <algorithm>
+
+// windows socket includes
+#include <WinSock2.h>
 
 
 
@@ -23,6 +28,38 @@ void PrintHelp()
 	std::cout << "-h or --help" << std::endl;
 }
 
+std::vector<std::string> ReadAddresses(std::string AddressFileName)
+{
+	std::vector<std::string> MACAddresses = std::vector<std::string>();
+	std::ifstream FID(AddressFileName);
+	if (FID.is_open)
+	{
+		std::string Address;
+		while (std::getline(FID,Address))
+		{
+			Address.erase(std::remove_if(Address.begin(), Address.end(), isspace), Address.end());
+			if (Address.size() > 0)
+				MACAddresses.push_back(Address);
+		}
+		FID.close;
+	}
+
+	return MACAddresses;
+}
+
+bool SendWakeOnLAN(const std::string MACAddress, unsigned PortAddress, unsigned long Broadcast)
+{
+
+	// Build message
+	// 6x 0xFF followed 16x MACAddress
+	std::string Message(6, 0xFF);
+	for (auto i = 0; i < 16; ++i)
+	{
+		Message += MACAddress;
+	}
+
+	// Create socket
+}
 
 int main(int argc, char* argv[])
 {
@@ -40,7 +77,21 @@ int main(int argc, char* argv[])
 
 
 	std::string MACFileName("MACAddresses.txt");
-	std::vector<std::string> MACAddressList = std::vector<std::string>();
+	std::vector<std::string> MACAddressList = ReadAddresses(MACFileName);
 
+	// Socket variables
+	WSADATA WsaData;
+	SOCKET SendingSocket = INVALID_SOCKET;
+
+	// Initialize WinSock
+	if (WSAStartup(MAKEWORD(2, 2), &WsaData) != 0)
+	{
+		std::cout << "WSA startup failed with error:" << std::endl;
+		std::cout << WSAGetLastError() << std::endl;
+	}
+	else
+	{
+
+	}
 
 }
